02a4bfc8c8190155913f2079958bc344bd3618a2
code-with-arafat <ferdousinfo00@gmail.com>
2026-08-03 16:40:23 +0600

    Explore JavaScript Math Object

---
+++ problem4.js
@@ -0,0 +1,21 @@
+//In JavaScript, Math is a built-in global namespace object that provides properties and methods for mathematical constants and functions.
+//find largest value 
+const numbers = [10,12,9,100,124,55,147,158,160,120,150]
+//Max value
+const largestNumber = Math.max(10,12,9,100,124,55,147,158,160,120,150)
+console.log(largestNumber)
+//Minimum value
+const lowestNumber = Math.min(10,12,9,100,124,55,147,158,160,120,150)
+console.log(lowestNumber)
+
+console.log(Math.PI);
+console.log(Math.random());
+console.log(Math.abs(12,12.5));
+console.log(Math.floor(9.99));
+console.log(Math.ceil(9.01));
+console.log(Math.pow(10,15));
+console.log(Math.round(3.40));
+console.log(Math.sqrt(25));
+console.log(Math.random()*10);
+
+
